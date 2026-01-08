39e4eca9466d0bb4a3fc7d2c562b1cca51f87f7f
hw9106 <hw9106@naver.com>
2026-01-08 11:47:47 +0900

    123

--- src/main/java/com/pro/app/controller/DefaultController.java
+++ src/main/java/com/pro/app/controller/DefaultController.java
@@ -45,8 +45,8 @@ public class DefaultController {
 
     @GetMapping(value = {"/hello","/"})
     public String hello(){
-        //return "Welcome Kubernetes Another Class  ["+ applicationVersion +"]\n";
-        return "안녕 Kubernetes Another Class  ["+ applicationVersion +"]\n";
+        return "Welcome Kubernetes Another Class  ["+ applicationVersion +"]\n";
+        //return "안녕 Kubernetes Another Class  ["+ applicationVersion +"]\n";
         //return "Bye Kubernetes Another Class  ["+ applicationVersion +"]\n";
         //return "Hello Kubernetes Another Class ["+ applicationVersion +"]\n";
         //return "Good Job Kubernetes Another Class ["+ applicationVersion +"]\n";
